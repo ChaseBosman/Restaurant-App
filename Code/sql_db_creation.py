81be027562d89e798608bece0cb0b2d37b0dc6ee
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-07-15 22:20:42 -0700

    Added comments for db creation
    Added commenting to include the purpopse of each execute statement for the build process of the database file.

--- Code/sql_db_creation.py
+++ Code/sql_db_creation.py
@@ -3,6 +3,7 @@ import sqlite3
 conn = sqlite3.connect('food.db')
 curs = conn.cursor()
 
+# this statement creates a table to hold information on each ticket/tab for each sale
 curs.execute("""CREATE TABLE ticket (
         ticket_id int NOT NULL,
         curr_date date NOT NULL,
@@ -10,6 +11,7 @@ curs.execute("""CREATE TABLE ticket (
             PRIMARY KEY (ticket_id, curr_date)
         )""")
 
+# this statement creates a table to hold information on each food item and its relation to a ticket
 curs.execute("""CREATE TABLE food_item_ordered (
         food_order_id PRIMARY KEY,
         ticket_id int NOT NULL,
@@ -19,6 +21,7 @@ curs.execute("""CREATE TABLE food_item_ordered (
             FOREIGN KEY (ticket_id, curr_date) REFERENCES ticket(ticket_id, curr_date)
         )""")
 
+# this statement creates a table to hold information on each drink item and its relation to a ticket
 curs.execute("""CREATE TABLE drink_item_ordered (
         drink_order_id PRIMARY KEY,
         ticket_id int NOT NULL,
@@ -28,11 +31,13 @@ curs.execute("""CREATE TABLE drink_item_ordered (
             FOREIGN KEY (drink_item_id) REFERENCES drink_item(drink_item_id)
         )""")
 
+# this statement creates a table to hold information of each food category
 curs.execute("""CREATE TABLE food_type (
         food_type_id PRIMARY KEY,
         type_name varchar(25) NOT NULL
         )""")
 
+# this statement creates a table to hold information of each food item
 curs.execute("""CREATE TABLE food_item (
         food_item_id PRIMARY KEY,
         food_name varchar(25) NOT NULL,
@@ -41,11 +46,13 @@ curs.execute("""CREATE TABLE food_item (
             FOREIGN KEY (food_type_id) REFERENCES food_type(food_type_id)
         )""")
 
+# this statement creates a table to hold information of each drink category
 curs.execute("""CREATE TABLE drink_type (
         drink_type_id PRIMARY KEY,
         type_name varchar(25) NOT NULL
         )""")
 
+# this statement creates a table to hold information of each drink item
 curs.execute("""CREATE TABLE drink_item (
         drink_item_id PRIMARY KEY,
         drink_name varchar(25) NOT NULL,
