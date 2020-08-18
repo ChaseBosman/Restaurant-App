7f739ef7b98642967d5a883df051d3a9be65af44
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-08-18 15:21:38 -0700

    Cleaned up category_window buttons

--- src/category_window.py
+++ src/category_window.py
@@ -5,7 +5,7 @@ from food_db_operations import FoodDbOperations
 class CategoryWindow(Toplevel):
     def __init__(self, master):
         self.top = Toplevel.__init__(self, master)
-        self.geometry("400x400")
+        self.geometry("350x200")
         self.populate_category_buttons()
 
     def populate_category_buttons(self):
@@ -21,8 +21,9 @@ class CategoryWindow(Toplevel):
             drinks = cur.fetchall()
             for row in drinks:
                 print(row[0])
-                self.category_buttons.append(Button(self, text=row[0]))
-                self.category_buttons[self.category_count_drinks].grid(row=self.category_count_drinks, column=0)
+                self.category_buttons.append(Button(self, text=row[0], padx=5, pady=5))
+                self.category_buttons[self.category_count_drinks].grid(sticky=N+S+E+W,
+                                                                       row=self.category_count_drinks, column=0)
                 self.category_count_drinks += 1
 
             # add food type buttons
@@ -30,8 +31,8 @@ class CategoryWindow(Toplevel):
             food = cur.fetchall()
             for row in food:
                 print(row[0])
-                self.category_buttons.append(Button(self, text=row[0]))
+                self.category_buttons.append(Button(self, text=row[0], padx=5, pady=5))
                 # add self.category_count_drinks to category_buttons index to account for button count in list
                 self.category_buttons[self.category_count_food+self.category_count_drinks].\
-                    grid(row=self.category_count_food, column=15)
+                    grid(sticky=N+S+E+W, row=self.category_count_food, column=25)
                 self.category_count_food += 1
