50b588b5e104585c37c97f2bf7638dba97c306f9
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-08-21 17:38:55 -0700

    Began creating socket communication classes
    Will utilize a client like setup to relay committed food to chefs. Chef's end of the socket will act as a server, recieving food items and managing them for chefs

---
+++ src/committed_client_window.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+
+class CommittedClientWindow:
+    def __init__(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            while True:
+                message = input("Enter data")
+                s.sendall(message.encode())
+
---
+++ src/committed_server_window.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+class CommittedServerWindow:
+    def __init__(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024).decode()
+                    if not data:
+                        break
+                    print(data)
+
+
+
+
+
