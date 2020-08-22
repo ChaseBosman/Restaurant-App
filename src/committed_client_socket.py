fd8137ce46c5d031c84f0322f1a5325f3c989caa
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-08-21 17:53:28 -0700

    Renamed to make more sense

--- src/committed_client_socket.py
+++ src/committed_client_socket.py
@@ -4,7 +4,7 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 
-class CommittedClientWindow:
+class CommittedClientSocket:
     def __init__(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
