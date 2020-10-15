970b94247aa0de0351ffeae435349f3c750bd3a3
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-10-14 21:32:54 -0700

    Create test_phone_number_window.py

---
+++ tests/test_phone_number_window.py
@@ -0,0 +1,33 @@
+import unittest
+import client.phone_number_window
+from tkinter import *
+
+
+class TestPhoneNumberWindow(unittest.TestCase):
+    def test_set_text(self):
+        """
+        Tests if the set text method works as needed for button entry for tables
+        """
+        root_window = Tk()
+        phone = client.phone_number_window.PhoneNumberWindow(root_window)
+        phone.set_text("H")
+        phone.set_text("e")
+        phone.set_text("l")
+        phone.set_text("l")
+        phone.set_text("o")
+        self.assertEqual(phone.phone_entry.get(), "Hello")
+
+    def test_del_text(self):
+        """
+        Tests if the delete method for the text window works properly
+        """
+        root_window = Tk()
+        phone = client.phone_number_window.PhoneNumberWindow(root_window)
+        phone.set_text("Hello")
+
+        self.assertEqual(phone.phone_entry.get(), "Hello")
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
